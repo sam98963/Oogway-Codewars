b89fb754a13b91712959aa929a4caeb6c7ccf700
sam98963 <szwilliamson01@gmail.com>
2023-04-08 11:41:47 +0100

    Kung Fu Codewars - Don't anger oogway

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
-/* 
-👉 Write your kata here!
+/*
+Master Oogway is full of wisdom and not shy of empathy. He will often give a suitably reassuring quote when an issue is presented to him. 
+
+However, recently Oogway has faced much criticism over his selection of the dragon warrior. He does not like to even hear the words, and will shun you if they are mentioned.
+
+Write a function that will take two inputs:
+  problem - the issue proposed to Oogway.
+  response - the exclamtory response to the problem.
+
+  The function must return a string from Oogway, with an exclamation mark to show his enthusiasm - however, if the string already ends with an exclamation mark, do not add another. If the dragon warrior is mentioned in any form, return "DO NOT QUESTION MY CHOICE OF THE DRAGON WARRIOR!"
+
+e.g. if "I lost my maths homework and got told off" is input as problem, return "There are no accidents!"
+e.g. if "I should be the new Dragon Warrior" is input as problem, return "DO NOT QUESTION MY CHOICE OF THE DRAGON WARRIOR!"
+e.g. if "WhY aM I nOt thE DrAGon WaRRiOR?" is input, return "DO NOT QUESTION MY CHOICE OF THE DRAGON WARRIOR!"
+
+Enjoy!
 */
 
-//👉 Write the function your CodeWarriors will start with below here:
+
+
+export function oogwayReply(problem, solution){
+
+  // Return a suitable answer here.
+  return solution
+}
--- main.test.js
+++ main.test.js
@@ -1,1 +1,47 @@
 //👉 Write your tests below here:
+import { test, expect } from "@jest/globals";
+import { oogwayReply } from "./main.js";
+
+// Test to see if input is a string
+// Test dragon warrior to return bad outcome ✅
+// Test to see if DrAGoN WaRRiOR works✅
+// Test to see if Dragon Warrior? works✅
+// Test return of normal problem ✅
+// Test to see if there is already an exclamation mark in response, if so do not add another. ✅
+// Test to see if input question is a string.
+
+
+
+
+
+describe('Testing for reply based on input', () => {
+  test('If it says "dragon warrior" return bad outcome', () => {
+    let actual = oogwayReply("I should be the new dragon warrior", "There are no accidents");
+    let expected = "DO NOT QUESTION MY CHOICE OF THE DRAGON WARRIOR!"
+    expect(actual).toBe(expected);
+  })
+
+  test('If dragon warrior has assorted capital letters "DrAGoN WarRioR" return bad outcome', () => {
+    let actual = oogwayReply("DrAGoN WarRioR Sucks!", "It matters not what someone is born, but what they grow to be");
+    let expected = "DO NOT QUESTION MY CHOICE OF THE DRAGON WARRIOR!"
+    expect(actual).toBe(expected);
+  })
+
+  test('If there is a symbol in the input, will it still return bad outcome?', () => {
+    let actual = oogwayReply("Why am I not the Dragon Warrior?", "Quit, don’t quit. Noodles, don’t noodles… You are too concerned with what was and what will be");
+    let expected = "DO NOT QUESTION MY CHOICE OF THE DRAGON WARRIOR!"
+    expect(actual).toBe(expected);
+  })
+
+  test('If it is genuine problem, return quote', () => {
+    let actual = oogwayReply("I lost my maths homework and got told off", "You just need to believe. You must believe");
+    let expected = "You just need to believe. You must believe!"
+    expect(actual).toBe(expected);
+  })
+
+  test('If quote ends with exclamation, do not add another', () => {
+    let actual = oogwayReply("I lost my maths homework and got told off", "There is always something more to learn. Even for a master!");
+    let expected = "There is always something more to learn. Even for a master!"
+    expect(actual).toBe(expected);
+  })
+})
